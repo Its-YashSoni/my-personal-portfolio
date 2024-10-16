3c9b121e3d9552f8690b77fea8d2f226733b7a61
Yash Soni <ys00soni36@gmail.com>
2024-10-16 12:59:49 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Founder @XPlatform Solutions","VP @SISTec Ratibad", "Flutter Developer", "ML Enthausiast"];
+const textArray = ["VP @SISTec Ratibad", "Flutter Developer", "ML Enthausiast"];
 const typingDelay = 100;
 const erasingDelay = 100;
 const newTextDelay = 500; // Delay between current and next text
